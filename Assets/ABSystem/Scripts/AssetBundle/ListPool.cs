51fc82e9a6b67a34f9f4f2ece77dce57fbd125dc
tangzx <272669294@qq.com>
2016-07-16 11:23:24 +0800

    object list pool

---
+++ Assets/ABSystem/Scripts/AssetBundle/ListPool.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+
+namespace Tangzx.ABSystem
+{
+    /// <summary>
+    /// 从UGUI源码中挪过来的
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    internal static class ListPool<T>
+    {
+        // Object pool to avoid allocations.
+        private static readonly ObjectPool<List<T>> s_ListPool = new ObjectPool<List<T>>(null, l => l.Clear());
+
+        public static List<T> Get()
+        {
+            return s_ListPool.Get();
+        }
+
+        public static void Release(List<T> toRelease)
+        {
+            s_ListPool.Release(toRelease);
+        }
+    }
+}
